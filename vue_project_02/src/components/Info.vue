86ef7a115af7c87fe37f68aa64f2eda7003c76d1
Daniela Maccali <daniela.maccali@gmail.com>
2022-07-04 18:56:03 -0300

    Componentes em árvores

---
+++ vue_project_02/src/components/Info.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Estou dormindo no momento.</p>
+        <p>Utilizo as seguintes tecnologias:</p>
+        <ul>
+            <li>Javascript</li>
+            <li>CSS</li>
+            <li>Vue</li>
+        </ul> 
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Info'
+    }
+</script>
---
+++ vue_project_02/src/components/LifeCycle.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Meu nome é: {{ nome }} </h1>
+</template>
+
+<script>
+    export default {
+        name: 'LyfeCycle',
+        data(){
+            return{
+                nome: 'Ainda não sei'
+            }
+        },
+        created() {
+            setTimeout(() => {
+                this.nome = 'Daniela'
+            }, 1000)
+            
+        },
+        mounted() {
+            setTimeout(() => {
+                this.nome = 'Luci'
+            }, 2000)
+            
+        }
+    }
+</script>
