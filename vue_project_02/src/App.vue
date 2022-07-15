de1a4ce132fcfce02725e6e9faa60ba1f042de00
Daniela Maccali <daniela.maccali@gmail.com>
2022-07-15 18:48:13 -0300

    CSS Scoped e CSS global + componente Header

--- vue_project_02/src/App.vue
+++ vue_project_02/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Header />
     <PrimeiroComponente />
     <LifeCycle />
     <Pessoa />
@@ -7,16 +8,30 @@
 </template>
 
 <script>
-import PrimeiroComponente from './components/PrimeiroComponente.vue'
-import LifeCycle from './components/LifeCycle.vue'
-import Pessoa from './components/Pessoa.vue'
+  import Header from './components/Header.vue'
+  import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import LifeCycle from './components/LifeCycle.vue'
+  import Pessoa from './components/Pessoa.vue'
 
-export default {
-  name: 'App',
-  components: {
-    PrimeiroComponente,
-    LifeCycle,
-    Pessoa
-}
-}
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      PrimeiroComponente,
+      LifeCycle,
+      Pessoa
+  }
+  }
 </script>
+
+<style>
+  body {
+    background: #333;
+    color: #FFF;
+  }
+
+  a {
+    color: #3d793d;
+  }
+
+</style>
---
+++ vue_project_02/src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Contato</a></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+    
+}
+</script>
+
+<style scoped>
+    ul {
+        list-style: none;
+        display: flex;
+        background: #cc7f0c;
+    }
+
+    li {
+        margin-right: 10px;
+    }
+
+    a {
+        color: #ddd;
+        text-decoration: none;
+        transition: .5s;
+    }
+
+    a:hover {
+        color: #7e3959;
+    }
+
+</style>
